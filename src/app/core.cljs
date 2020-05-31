7214c719a7d90cf120021af76153804a26200efe
Lucas de Senna <lucasdesenna@gmail.com>
2020-05-31 20:51:36 -0300

    REFAC: Update app modules references

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,7 +1,7 @@
 (ns app.core
   "This namespace contains your application and is the entrypoint for 'yarn start'."
   (:require [reagent.core :as r]
-            [app.hello :refer [hello]]))
+            [app.modules.hello.components :refer [hello]]))
 
 (defn ^:dev/after-load render
   "Render the toplevel component for this app."
